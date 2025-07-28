aa68ad204fdec942e4c5cfb0e8a896c897f3fa98
tseminc <tseminc.ios@gmail.com>
2025-07-28 18:57:35 +0800

    modify 2

--- Lesson6/Lesson6_1_v2.py
+++ Lesson6/Lesson6_1_v2.py
@@ -2,12 +2,12 @@ import asyncio
 from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
 
 async def main():
-    #url = 'https://www.wantgoo.com/stock/2330/technical-chart'
-    url = 'https://www.wantgoo.com/stock/2317/technical-chart'
+    url = 'https://www.wantgoo.com/stock/2330/technical-chart'
+    #url = 'https://www.wantgoo.com/stock/2317/technical-chart'
     #建立一個BrowserConfig,讓chromium的瀏覽器顯示
     #BrowserConfig實體
     browser_config = BrowserConfig(
-        headless=True
+        headless=False
     )
     # 建立一個AsyncWebCrawler的實體，並傳入BrowserConfig實體
     # 這樣可以讓爬蟲等待瀏覽器載入頁面，並且可以在瀏覽器中看到爬蟲的操作，方便除錯
@@ -15,8 +15,8 @@ async def main():
         wait_for_images=True,  # 等待圖片載入
         scan_full_page=True,  # Tells the crawler to try scrolling the entire page
         scroll_delay=0.5,     # Delay (seconds) between scroll steps
-        page_timeout=800000,               # 80秒超時
-        js_code=["document.querySelector('.my-drawer-toggle-btn').click();"],
+        #page_timeout=800000,               # 80秒超時
+        #js_code=["document.querySelector('.my-drawer-toggle-btn').click();"],
         cache_mode=CacheMode.BYPASS,
         verbose=True
     )
